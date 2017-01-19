c74e9e1ebcee2a0ad0f2321931959bd179ed4a03
luluskana <lulus.kana@gmail.com>
2017-01-19 13:11:52 +0700

    new project

---
+++ src/main/java/com/foamtec/OtApplication.java
@@ -0,0 +1,12 @@
+package com.foamtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtApplication.class, args);
+	}
+}
